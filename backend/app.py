37fc67762ef992ada1e39bb13e77003f02917094
Matt Dailey <mattddailey@gmail.com>
2022-10-12 12:38:51 -0400

    Modify Environment

--- backend/app.py
+++ backend/app.py
@@ -4,25 +4,25 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+BASE_URL = os.getenv('BASE_URL')
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 
 SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
-
-CALLBACK_URL = "http://localhost:5000/callback"
+SPOTIFY_SCOPE = "user-read-playback-state" 
+CALLBACK_URL = "{}:5000/callback".format(BASE_URL)
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-	data = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, SPOTIFY_CLIENT_ID, CALLBACK_URL, "user-read-playback-state") 
+	data = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, SPOTIFY_CLIENT_ID, CALLBACK_URL, SPOTIFY_SCOPE) 
 	return redirect(data)
 
 @app.route('/callback')
 def callback():
-	return redirect('http://raspberrypi.local:3000/')
+	return redirect("{}:3000/".format(BASE_URL))
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
