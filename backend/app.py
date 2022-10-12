eadc1f3f2c406677b956818b9ef193768429fa8f
Matt Dailey <mattddailey@gmail.com>
2022-10-12 08:06:40 -0400

    Add initial spotify auth

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_cors import CORS
+
+CLIENT_ID = "50812dc26802415491ccf6bc1772c000"
+CLIENT_SECRET = "d70f9b62f1d1482ca9a4d7194f2ba7e1"
+SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
+
+REDIRECT_URI = "http://localhost:5000/callback"
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def index():
-	return 'Hello World'
+	data = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, CLIENT_ID, REDIRECT_URI, "user-read-playback-state") 
+	return redirect(data)
+
+@app.route('/callback')
+def callback():
+	return redirect('http://localhost:3000/')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
