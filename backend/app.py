79acdfa45957c28c818e7ea070797e749e527805
Matt Dailey <mattddailey@gmail.com>
2022-10-14 17:16:06 -0400

    Change return

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
@@ -12,17 +12,24 @@ SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
 SPOTIFY_SCOPE = "user-read-playback-state" 
 CALLBACK_URL = "{}:5000/callback".format(BASE_URL)
 
+AUTHORIZATION_CODE = ""
+
 app = Flask(__name__)
 CORS(app)
 
+
+
 @app.route('/')
 def index():
-	data = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, SPOTIFY_CLIENT_ID, CALLBACK_URL, SPOTIFY_SCOPE) 
-	return redirect(data)
+	# Redirect Spotify Authorization URL
+	authURL = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, SPOTIFY_CLIENT_ID, CALLBACK_URL, SPOTIFY_SCOPE) 
+	return redirect(authURL)
 
 @app.route('/callback')
 def callback():
-	return redirect("{}:3000/".format(BASE_URL))
+	# Upon successful callback from Sptofify Authorization, redirect to the React App Homepage
+	# return redirect("{}:3000/".format(BASE_URL))
+	return "Hello from dietpi"
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
