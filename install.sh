825083c7257321f4203d5cfe4b31899054281e55
Matt Dailey <mattddailey@gmail.com>
2022-10-16 09:30:14 -0400

    Install script formatting

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+echo "$(tput setaf 4)Installing python dependencies...$(tput init)"
+
 sudo pip3 install -r backend/requirements.txt
 
 git submodule update --init --recursive
 git config matrix.ignore all
 
-echo "$(tput setaf 4)Running rgbmatrix installation...$(tput setaf 9)"
+echo "$(tput setaf 4)Running rgbmatrix installation...$(tput init)"
 
 cd matrix/bindings/python/rgbmatrix/ || exit
 
@@ -12,15 +14,19 @@ python3 -m cython -2 --cplus *.pyx
 
 cd ../../../ || exit
 
+echo "$(tput setaf 4)Running Make in matrix directory...$(tput init)"
+
 make build-python PYTHON="$(command -v python3)"
 sudo make install-python PYTHON="$(command -v python3)"
 
 cd ../ || exit
 
+echo "$(tput setaf 4)Bringing git up to date...$(tput init)"
+
 git reset --hard
 git fetch origin --prune
 git pull
 
-echo "$(tput setaf 4)Running Make...$(tput setaf 9)"
+echo "$(tput setaf 4)Running Make at root directory...$(tput init)"
 
 make
