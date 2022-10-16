857d70af42c32ad0f03f9aed7002c1ba9064443e
Matt Dailey <mattddailey@gmail.com>
2022-10-16 15:40:41 +0100

    Update start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,12 @@
+echo "$(tput setaf 4)Starting react app...$(tput init)"
+
+cd frontend || exit
+
+npm start &
+
 echo "$(tput setaf 4) Starting celery...$(tput init)"
 
-cd backend || exit
+cd ../backend || exit
 
 celery -A app.celery worker --loglevel=info  &
 
