631a085f5ad1628e4243a48d7f4c836834801b6b
Matt Dailey <mattddailey@gmail.com>
2022-10-16 15:41:51 +0100

    Update start script to be able to run anywhere

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,14 @@
+# Make script work regardless of where it is run from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${DIR}/.." || exit
+
 echo "$(tput setaf 4)Starting react app...$(tput init)"
 
 cd frontend || exit
 
 npm start &
 
-echo "$(tput setaf 4) Starting celery...$(tput init)"
+echo "$(tput setaf 4)Starting celery...$(tput init)"
 
 cd ../backend || exit
 
