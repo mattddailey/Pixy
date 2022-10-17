b10e0c1cee4d3d03c88be659b7be2297440db3ea
Matt Dailey <mattddailey@gmail.com>
2022-10-17 22:58:46 +0100

    Start Redis server in script

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,6 +12,8 @@ echo "$(tput setaf 4)Starting celery...$(tput init)"
 
 cd ../backend || exit
 
+
+redis-server &
 celery -A app.celery worker --loglevel=info  &
 
 echo "$(tput setaf 4)Starting flask server...$(tput init)"
