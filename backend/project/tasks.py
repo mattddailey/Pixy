7fd501914305541f6fa8812ab80e7c16c02b4a9a
Matt Dailey <mattddailey@gmail.com>
2022-11-13 17:59:39 +0000

    Working docker containerization

--- backend/project/tasks.py
+++ backend/project/tasks.py
@@ -7,14 +7,14 @@ from celery.contrib.abortable import AbortableTask
 from celery.utils.log import get_task_logger
 
 from project.spotify import Spotify
-# from project.matrix_manager import MatrixManager
+from project.matrix_manager import MatrixManager
 
 logger = get_task_logger(__name__)
 
 @shared_task(bind=True, base=AbortableTask)
 def display_spotify_album_art(self, access_token, refresh_token, token_expire_timestamp, base=AbortableTask):
 	spotify_api = Spotify(access_token=access_token, refresh_token=refresh_token, token_expire_timestamp=token_expire_timestamp)
-	# matrix_manager = MatrixManager() 
+	matrix_manager = MatrixManager() 
 	
 	url_to_display = None
 	image_url = None
@@ -30,7 +30,7 @@ def display_spotify_album_art(self, access_token, refresh_token, token_expire_ti
 			if image_url != url_to_display:
 				logging.info("Displaying new album cover")
 				url_to_display = image_url
-				# matrix_manager.display_image(url_to_display)
+				matrix_manager.display_image(url_to_display)
 		time.sleep(1)
 
 		
