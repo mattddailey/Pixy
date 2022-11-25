37e03c052e7d4288396e58a3689c52fc7447a221
Matt Dailey <mattddailey@gmail.com>
2022-11-25 18:25:09 -0500

    Remove comment

--- backend/model/mode.py
+++ backend/model/mode.py
@@ -5,8 +5,3 @@ class Mode:
         self.mode = mode
         # Optional properties
         self.authorization_code = authorization_code
-
-# class Spotify(Mode):
-#     def __init__(self, authorization_code: str, mode: str = ModeType.SPOTIFY.value):
-#         self.authorization_code = authorization_code
-#         super().__init__(mode)
